8d1f57217b9864aa5e171502a73ba03ae362b89d
Nick <Ecton.Nick@gmail.com>
2018-09-05 21:08:19 -0700

    W3D3 Homework

--- W3D3/demo_project/app/models/Person.rb
+++ W3D3/demo_project/app/models/Person.rb
@@ -1,10 +1,10 @@
 class Person < ActiveRecord::Base
-  belongs_to :house, {
+  belongs_to :house, 
     class_name: 'House',
     foreign_key: :house_id,
     primary_key: :id,
     optional: true
-  }
+
   # validates :name, :house_id
   # def up
   #   create_table :people do |t|
